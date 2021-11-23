3bf89f620c588d1c92841466084906e739c65156
kimjihw <kimal846@naver.com>
2021-11-24 01:39:56 +0900

    clear

---
+++ Level 1/실패율.py
@@ -0,0 +1,18 @@
+import collections
+
+def solution(N, stages):
+    answer = {}
+    player = len(stages)
+    for i in range(1, N + 1):
+        if player != 0:
+            answer[i] = stages.count(i) / player
+            player -= stages.count(i)
+        else:
+            answer[i] = 0
+    answer = sorted(answer, key = lambda x : -answer[x])
+    print(answer)
+    return answer
+
+N = 5
+stages = [2,1,2,6,2,4,3,3]
+solution(N, stages)
