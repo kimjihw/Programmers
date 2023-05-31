e12e0dcb94f57ed5ac23f58f762fade4b15620f4
jihwan <kimal846>
2023-05-31 15:14:58 +0900

    clear

---
+++ Level 0/배열에서 문자열 대소문자 변환하기.py
@@ -0,0 +1,13 @@
+def solution(strArr):
+    answer = []
+
+    for i in range(len(strArr)):
+        if i % 2 == 0:
+            answer.append(strArr[i].lower())
+        else:
+            answer.append(strArr[i].upper())
+
+    return answer
+
+
+solution(["AAA", "BBB", "CCC", "DDD"])
