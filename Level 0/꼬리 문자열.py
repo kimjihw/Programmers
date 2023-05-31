37c07e7494d24d7fed76433db144a285e4fab911
jihwan <kimal846>
2023-05-31 14:43:25 +0900

    Revert "clear"
    This reverts commit c28d741ffed02f7c6cfad78aa4427740235435b0.

--- Level 0/꼬리 문자열.py
+++
@@ -1,9 +0,0 @@
-def solution(str_list, ex):
-    answer = ''
-    for i in str_list:
-        if ex not in i:
-            answer += i
-
-    return answer
-
-solution(["abc", "bbc", "cbc"], "c")
--- Level 0/순서 바꾸기.py
+++
@@ -1,7 +0,0 @@
-def solution(num_list, n):
-    answer = []
-    lst = num_list[n::]
-    answer = lst + num_list[:n]
-    return answer
-
-solution([5, 2, 1, 7, 5], 3)
