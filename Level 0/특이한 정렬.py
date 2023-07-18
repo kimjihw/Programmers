670127cb2f04a9e7b5fa6968a31f2e91c3f56468
지환 <57333984+kimjihw@users.noreply.github.com>
2023-07-18 22:54:49 +0900

    clear

---
+++ Level 0/특이한 정렬.py
@@ -0,0 +1,13 @@
+def solution(numlist, n):
+    result = sorted(numlist, key=lambda x : (abs(x-n), (n-x)))
+    # 튜플로 변환하여 정렬
+    # x = 1, key = (abs(1-4), (4-1)) = (3, 3)
+    # x = 2, key = (abs(2-4), (4-2)) = (2, 2)
+    # x = 3, key = (abs(3-4), (4-3)) = (1, 1)
+    # x = 4, key = (abs(4-4), (4-4)) = (0, 0)
+    # x = 5, key = (abs(5-4), (4-5)) = (1, -1)
+    # x = 6, key = (abs(6-4), (4-6)) = (2, -2)
+    return result
+
+
+solution([1, 2, 3, 4, 5, 6], 4)
